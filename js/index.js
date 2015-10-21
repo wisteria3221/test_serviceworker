1aa4d91eca8cd47858fcc638d592fca037d5604f
藤原 侑亮 <y_hujiwara@waku-2.com>
2015-10-21 17:26:28 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 (function() {
 	"use strict";
 
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/test_serviceworker/sw.js').then(function(registration) {
+	if (location.protocol == "http:") {
+		location.replace(location.href.replace("http:", "https:"));
+		return;
+	}
+
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("/test_serviceworker/sw.js").then(function(registration) {
 			// 登録成功
-			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			console.log("ServiceWorker registration successful with scope: ", registration.scope);
 		}).catch(function(err) {
 			// 登録失敗
-			console.warn('ServiceWorker registration failed: ', err);
+			console.warn("ServiceWorker registration failed: ", err);
 		});
 	}
 })();
--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 // installイベント
-self.addEventListener('install', function(e) {
-	console.info('>>>>> onInstall <<<<<')
+self.addEventListener("install", function(e) {
+	console.info(">>>>> onInstall <<<<<")
 });
 
 // activateイベント
-self.addEventListener('activate', function(e) {
-	console.info('>>>>> onActivate <<<<<')
+self.addEventListener("activate", function(e) {
+	console.info(">>>>> onActivate <<<<<")
 });
 
 // pushイベント
 self.addEventListener("push", function(e) {
-	console.info('>>>>> onPush <<<<<')
+	console.info(">>>>> onPush <<<<<")
 
 	e.waitUntil(
 		self.registration.showNotification("Push Notification Title", {
