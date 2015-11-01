26b94398011d4c4067b32abd975611340c9a4b27
wisteria3221 <wisteria3221@hotmail.co.jp>
2015-11-01 18:09:32 +0900

    update_20151101_5

--- js/index.js
+++ js/index.js
@@ -185,63 +185,6 @@
 		});
 	}
 
-}());
-
-
-
-
-
-
-
-(function() {
-	"use strict";
-	return;
-
-	if (location.protocol == "http:") {
-		location.replace(location.href.replace("http:", "https:"));
-		return;
-	}
-
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("/test_serviceworker/sw.js").then(function(registration) {
-			// 登録成功
-			console.log("ServiceWorker registration successful with scope: ", registration.scope);
-		}).catch(function(error) {
-			// 登録失敗
-			var message = "ServiceWorker registration failed: " + error;
-			console.warn(message);
-			$("#result").text(message);
-		});
-	} else {
-		alert("ブラウザが \"ServiceWorker\" に対応していません");
-	}
-
-	// Push通知を初期化
-	initialisePushNotification();
-
-	/**
-	 * Push通知の初期設定
-	 */
-	function initialisePushNotification() {
-		// ServiceWorkerが通知に対応しているか判定
-		if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
-			alert("\"ServiceWorker\" が通知に対応していません");
-			return;
-		}
-
-		// 通知が拒否されているか判定
-		if (Notification.permission === 'denied') {
-			alert("通知が拒否しています");
-			return;
-		}
-
-		// ブラウザがPushAPIに対応しているか判定
-		if (!('PushManager' in window)) {
-			alert("ブラウザが \"Push API\" に対応していません");
-			return;
-		}
-	}
-
 	function showResultMessage(message, isError) {
 		var $messageElement = null;
 
@@ -253,4 +196,5 @@
 		$messageElement.text(message);
 		$("#result").append($messageElement);
 	}
-})();
+
+}());
